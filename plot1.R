cd8509c48bef7089dba59b62e93ec385025e5b27
Georgios-K <Georgios__k@outlook.com>
2015-04-26 16:57:24 +0300

    Plot1

--- plot1.R
+++ plot1.R
@@ -8,10 +8,10 @@ SCC <- readRDS("Source_Classification_Code.rds")
 # Divide by 1000 in order to present a more user friendly graph
 NEI$Emissions <- NEI$Emissions/1000
 
-aggregate.data <- with(NEI, aggregate(Emissions, by = list(year), sum))
+aggr_data <- with(NEI, aggregate(Emissions, by = list(year), sum))
 
 # plot 1 file
 png("plot1.png")
-plot(aggregate.data, type = "o", col="Red", ylab = expression("Emissions, PM"[2.5]), 
+plot(aggre_data, type = "o", col="Red", ylab = expression("Emissions, PM"[2.5]), 
      xlab = "Year", main = "Total Emissions in USA (.000)")
 dev.off()
